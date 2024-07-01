b0da4fa9f44aaf3dbbfe002c8db9368b339269d6
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-07-02 00:29:59 +0300

    id_pac_setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ToDo_App_With_React
- 
+
+Format On Paste
+Format On Save
+
+npm i uuid paketi kurduk.
--- todo_app/src/Components/TodoWrapper.js
+++ todo_app/src/Components/TodoWrapper.js
@@ -5,7 +5,11 @@ const TodoWrapper = () => {
     let [todos, setTodos] = useState([]); //useState([])= başlangıç degeri boş dizidir. //Tüm todo ların tutulacağı yer.
     
     const addTodo = todo => { //kullanıcı ekleye bastığı zaman çalışacak.
-        setTodos()
+        setTodos([...todos,{ //todos içerisindeki herşey,
+            id: 1,
+            desc:todo, //kullanıcı ne yazmışsa gel onu yaz dedik.
+            completed:false //görev yeni eklendiğinden tamamlanmamıştır yanı false.
+        }]) 
     };
 
     return (
