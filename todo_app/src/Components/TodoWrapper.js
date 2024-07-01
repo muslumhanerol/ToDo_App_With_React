5f31bce4cfaa9110f72cfce0ea3ece39589a9d58
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-07-01 23:24:56 +0300

    css_++

--- todo_app/src/Components/TodoWrapper.js
+++ todo_app/src/Components/TodoWrapper.js
@@ -3,7 +3,9 @@ import React from 'react'
 const TodoWrapper = () => {
     return (
         <>
-            <h1>Todo App</h1>
+            <div className='TodoWrapper'>
+                <h1>Todo App</h1>
+            </div>
         </>
     )
 }
