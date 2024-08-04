509044aabf40bd1f20e28923bde4329323be974c
muslumhanerol <132482365+muslumhanerol@users.noreply.github.com>
2024-08-04 23:25:23 +0300

    Update TodoWrapper.js

--- todo_app/src/Components/TodoWrapper.js
+++ todo_app/src/Components/TodoWrapper.js
@@ -7,7 +7,7 @@ import TodoContainer from './TodoContainer';
 const TodoWrapper = () => {
     let [todos, setTodos] = useState([]); //useState([])= başlangıç degeri boş dizidir. //Tüm todo ların tutulacağı yer.
 
-    const addTodo = todo => { //kullanıcı ekleye bastığı zaman çalışacak.
+    const addTodo = todo => { //yeni todo ekleme.
         setTodos([...todos, { //todos içerisindeki herşey,
             id: uuidv4(),
             desc: todo, //kullanıcı ne yazmışsa gel onu yaz dedik.
